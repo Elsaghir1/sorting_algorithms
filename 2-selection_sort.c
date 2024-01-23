025f091643bc92ccdc7b014490c85161f52202cf
Adham <adhamelsa8er@gmail.com>
2024-01-22 17:25:25 -0800

    a

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,23 +10,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int *min, tmp;
-	size_t i, j;
+	int tmp;
+	size_t i, j, k;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || !size)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array + i;
-		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
-
-		if ((array + i) != min)
+		for (j = size - 1, k = i + 1; j > i; j--)
+		{
+			if (array[j] < array[k])
+				k = j;
+		}
+		if (array[i] > array[k])
 		{
 			tmp = array[i];
-			array[i] = min;
-			min = tmp;
+			array[i] = array[k];
+			array[k] = tmp;
 			print_array(array, size);
 		}
 	}
